b988f1cf604c9ab4c1432f4dfa68baab57e67d9f
VarunSinha07 <varunsinha2604@gmail.com>
2024-12-30 22:24:13 +0530

    changes

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,10 @@
 import { createAuthClient } from "better-auth/react"
+import { magicLinkClient } from "better-auth/client/plugins"
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL // the base url of your auth server
+    baseURL: process.env.BETTER_AUTH_URL, // the base url of your auth server
+    plugins: [
+        magicLinkClient()
+    ]
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
- 
+import { magicLink } from "better-auth/plugins/magic-link";
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -10,4 +10,12 @@ export const auth = betterAuth({
     emailAndPassword: {  
         enabled: true
     },
+    plugins: [
+        magicLink({
+            sendMagicLink: async ({ email, token, url }, request) => {
+                // send email to user
+            }
+        })
+    ]
+
 });
