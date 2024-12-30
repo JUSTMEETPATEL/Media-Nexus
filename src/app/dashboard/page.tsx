2b32abf697815131ab63d6c4d8c4a30f078a14bf
VarunSinha07 <varunsinha2604@gmail.com>
2024-12-31 01:06:26 +0530

    changes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1>Dashboard</h1>
+        </div>
+    );
+};
+
+export default DashboardPage;
