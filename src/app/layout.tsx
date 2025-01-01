3201a038ddc2143ed867ea85e0681b50c33cfaaf
JUSTMEETPATEL <justmeetpatel@gmail.com>
2025-01-01 19:08:28 +0530

    metadata updated and seo done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,35 @@ export const metadata: Metadata = {
   title: 'Media Nexus',
   description:
     'Welcome to MEDIA NEXUS, where we offer specialized Certificate Programs in Media Studies designed to empower the next generation of media professionals. Our courses are tailored to provide a deep understanding of key areas such as journalism, digital media, content creation, media production, and communication strategies. With a focus on practical learning, we ensure that our students gain hands-on experience and develop the necessary skills to excel in the fast-paced media industry.',
+  keywords: [
+    'Media Studies',
+    'Certificate Programs',
+    'Journalism',
+    'Digital Media',
+    'Content Creation',
+    'Media Production',
+    'Communication Strategies',
+  ],
+  authors: [{ name: 'Media Nexus Team' }],
+  viewport: 'width=device-width, initial-scale=1.0',
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  // verification: {
+  //   google: 'your-google-site-verification',
+  // },
+  alternates: {
+    canonical: 'https://media-nexus.vercel.app',
+  },
 };
 
 export default function RootLayout({
@@ -33,7 +62,9 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
-        {!['/dashboard', '/faculty', '/admin'].some(path => window.location.pathname.startsWith(path)) && <BookEnquiryButton />}
+        {!['/dashboard', '/faculty', '/admin'].some((path) =>
+          window.location.pathname.startsWith(path)
+        ) && <BookEnquiryButton />}
         <Toaster />
       </body>
     </html>
