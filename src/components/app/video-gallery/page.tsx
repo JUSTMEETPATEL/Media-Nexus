03c6dbbd4d083bb4a0b8e097f61b59be57d477ac
JUSTMEETPATEL <justmeetpatel@gmail.com>
2025-01-12 00:21:33 +0530

    Changes

--- src/components/app/video-gallery/page.tsx
+++
@@ -1,9 +0,0 @@
-import { VideoGallery } from './components/video-gallery';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 ">
-      <VideoGallery />
-    </main>
-  );
-}
