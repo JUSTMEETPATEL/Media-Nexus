29ca8764c62b3cc1ebac8211b5df8ac4202621f6
JUSTMEETPATEL <justmeetpatel@gmail.com>
2025-02-08 18:27:20 +0530

    Docker added

--- src/app/temp/page.tsx
+++ src/app/temp/page.tsx
@@ -1,44 +1,52 @@
-import { Button } from '@/components/ui/button'
-import { authClient } from '@/lib/auth-client'
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { toast } from '@/hooks/use-toast';
+import { authClient } from '@/lib/auth-client';
+
+import React from 'react';
 
 const Home = () => {
   const handleSubmit = async (formData: FormData) => {
-    'use server'
+    'use server';
 
     const { error } = await authClient.signIn.magicLink({
-          email: formData.get('email') as string,
-          callbackURL: '/dashboard' //redirect after successful login (optional)
+      email: formData.get('email') as string,
+      callbackURL: '/dashboard',
     });
 
-    console.log(error);
-  }
+    if (error) {
+      toast({
+        title: 'Error',
+      });
+    } else {
+      toast({ title: 'Magic link sent to your email' });
+    }
+  };
 
   return (
-    <div className='pt-32'>
-    <form action={handleSubmit} className="max-w-md mx-auto mt-8">
+    <div className="pt-32">
+      <form action={handleSubmit} className="max-w-md mx-auto mt-8">
         <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 mb-2">
-                Email Address
-            </label>
-            <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter your email"
-                required
-            />
+          <label htmlFor="email" className="block text-gray-700 mb-2">
+            Email Address
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter your email"
+            required
+          />
         </div>
         <Button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600"
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600"
         >
-            Submit
+          Submit
         </Button>
-    </form>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
